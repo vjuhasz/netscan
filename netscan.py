22107fd0add44cbc98f42e4303301c2a0850f7c8
Viktor Juhasz <viktor.juhasz@thirdbridge.com@CL-COGNO379s-MA.local>
2017-06-29 15:49:21 +0100

    fix

--- netscan.py
+++ netscan.py
@@ -18,11 +18,13 @@ def callback_result(host, scan_result):
         print (host, " DOWN")
         result.append([host,'DOWN'])
     
-    for i in range(len(result)):
-        print result[i][0]
-        print result[i][1]
+
 
 nm.scan('10.10.101.0/28', arguments="-sP", callback=callback_result)
 while nm.still_scanning():
     #print("Waiting >>>")
     nm.wait(1)
+
+for i in range(len(result)):
+        print result[i][0]
+        print result[i][1]
