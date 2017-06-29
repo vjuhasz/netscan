842a45f20ecd2948a40957a7c396dc4db228ff55
Viktor Juhasz <viktor.juhasz@thirdbridge.com@CL-COGNO379s-MA.local>
2017-06-29 21:09:36 +0100

    added natsort

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
 import nmap
+import operator
+from natsort import natsorted
+
 nm = nmap.PortScanner()
-nm.scan(hosts='10.10.101.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
+nm.scan(hosts='10.10.101.0/24', arguments='-n -v -sn')
 hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
 for host, status in hosts_list:
+    print('{0}:{1}'.format(host, status))
+
+sorted_list = natsorted(hosts_list, key=lambda host: host[0])
+
+print "sorted"
+
+for host, status in sorted_list:
     print('{0}:{1}'.format(host, status))
