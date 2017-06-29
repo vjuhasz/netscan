d9bdec85d353ba6ba483fc85f2d35d039e890951
Viktor Juhasz <viktor.juhasz@thirdbridge.com@CL-COGNO379s-MA.local>
2017-06-29 15:47:40 +0100

    added list

--- netscan.py
+++ netscan.py
@@ -3,17 +3,26 @@
 import nmap
 import json
 
+
 nm = nmap.PortScannerAsync()
 def callback_result(host, scan_result):
 #    print (host, scan_result)
 #    print (host, scan_result['nmap']['scanstats']['uphosts'])
+    result = []
     isUP = scan_result['nmap']['scanstats']['uphosts']
 #    print (isUP)
     if isUP == '1':
         print (host, " UP")
+        result.append([host,'UP'])
     else:
         print (host, " DOWN")
-nm.scan('10.10.101.0/24', arguments="-sP", callback=callback_result)
+        result.append([host,'DOWN'])
+    
+    for i in range(len(result)):
+        print result[i][0]
+        print result[i][1]
+
+nm.scan('10.10.101.0/28', arguments="-sP", callback=callback_result)
 while nm.still_scanning():
     #print("Waiting >>>")
     nm.wait(1)
