7898c82f62d78a6eb0abde048f2d8f682ad792a1
Viktor Juhasz <viktor.juhasz@thirdbridge.com@CL-COGNO379s-MA.local>
2017-06-29 16:32:07 +0100

    fix

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 import nmap
 
+nm = nmap.PortScannerAsync()
 nm.scan(hosts='10.10.101.0/30', arguments='-n -sP -PE -PA21,23,80,3389')
 hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 for host, status in hosts_list:
