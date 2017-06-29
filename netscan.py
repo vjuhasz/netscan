9a36bb6cff02f035f88c3c61c7d1ad758774e13b
Viktor Juhasz <viktor.juhasz@thirdbridge.com@CL-COGNO379s-MA.local>
2017-06-29 14:05:46 +0100

    Added netscan.py

---
+++ netscan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import nmap
+import json
+
+nm = nmap.PortScannerAsync()
+def callback_result(host, scan_result):
+#    print (host, scan_result)
+#    print (host, scan_result['nmap']['scanstats']['uphosts'])
+    isUP = scan_result['nmap']['scanstats']['uphosts']
+#    print (isUP)
+    if isUP == '1':
+        print (host)
+nm.scan('10.10.101.0/24', arguments="-sP", callback=callback_result)
+while nm.still_scanning():
+    print("Waiting >>>")
+    nm.wait(2)
