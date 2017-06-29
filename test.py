bc23d6a60d49099479f4ba7d7e8b87e6c3d9cbe2
Viktor Juhasz <viktor.juhasz@thirdbridge.com@CL-COGNO379s-MA.local>
2017-06-29 16:31:24 +0100

    added test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import nmap
+
+nm.scan(hosts='10.10.101.0/30', arguments='-n -sP -PE -PA21,23,80,3389')
+hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+for host, status in hosts_list:
+    print('{0}:{1}'.host)
